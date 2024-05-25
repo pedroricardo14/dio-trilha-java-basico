4f2b7fc551c3c22aa4c9d785f826f7a1744acbba
pedroricardo14 <pedroricardo1032@gmail.com>
2024-05-25 09:22:29 -0300

    Tipos e Variáveis -3

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        // Casting: pega um tipo mais abrangente e converte para um tipo mais específico pois é o tipo correspondente.
+        // Não é legal fazer essa adaptação o tempo todo, mas se você quer definir um valor de precisão maior para precisão menor, você garante que se acontecer algo inesperado, você está ciente do ocorrido.
     }
 }
