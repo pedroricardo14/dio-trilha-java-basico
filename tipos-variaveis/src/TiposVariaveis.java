b363f621e4dafa9e456766e678f6da0ad1e267e9
pedroricardo14 <pedroricardo1032@gmail.com>
2024-05-25 09:32:49 -0300

    Anatomia das classes -6

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
+
     public static void main(String[] args) throws Exception {
+
+        // tipos primitivos 
+    // estude a classe String que representa texto na aplicação
+
+        String meuNome = "PEDRO RICARDO";
+
         double salarioMinimo = 2500;
 
         short numeroCurto = 1;
@@ -7,5 +14,11 @@ public class TiposVariaveis {
         short numeroCurto2 = (short) numeroNormal;
         // Casting: pega um tipo mais abrangente e converte para um tipo mais específico pois é o tipo correspondente.
         // Não é legal fazer essa adaptação o tempo todo, mas se você quer definir um valor de precisão maior para precisão menor, você garante que se acontecer algo inesperado, você está ciente do ocorrido.
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
