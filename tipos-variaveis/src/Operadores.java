89f6c6dc517620df845d4a58d0abf8740a7533c7
pedroricardo14 <pedroricardo1032@gmail.com>
2024-05-29 10:44:09 -0300

    Operadores -4

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -30,7 +30,7 @@ public class Operadores {
 
         // Cada etapa dessa foi para mostrar como funciona a concatenação de int com string, vice e versa.
 
-    */
+    
 
     int numero = 5;
     numero = - numero;
@@ -41,6 +41,22 @@ public class Operadores {
     // numero = + numero não funciona, pois o + é de uma função aritmética. Se quisermos converter, precisamos multiplicar por -1
     System.out.println(numero);
 
+    */
+   // int numero = 5;
+    // x repeticao 
+    //numero = numero + 2;
+   // numero++;
+    // numero ++ é o mesmo que numero +1
+   // System.out.println(++numero);
+    // No caso acima, ele primeiro mostrou o número e depois incrementou, então só vai mostrar o valor incrementado se fizer outro System.out.println(numero);
+    // Se você quer primeiro incrementar e depois imprimir, faça Systems.out.println(++numero);
+    // A mesma regra funciona para decremenetar
+    //System.out.println(numero);
 
+    boolean variavel = true;
+    //System.out.println(!variavel);
+    variavel = !variavel;
+    System.out.println(variavel);
+    // Esses são modos de converter um booleano de true para false e vice e versa.
     }
 }
